99e254464bb868892ae7dbaf994b238ca06b5aec
wangi <lee.kindness@inovageo.com>
2020-04-27 23:26:06 +0100

    use output dir for region info file

--- bin/simplifiedAdminPolygons.pl
+++ bin/simplifiedAdminPolygons.pl
@@ -124,7 +124,7 @@ foreach my $way ( values %{$ctx->{_Way}} ){
 	push @{$hSharedBorders->{$key}}, $way->{'id'};
 }
 
-writeRegionInfo( $ctx, "C:/usr/MapView/tmp/admin_regions.json" );
+writeRegionInfo( $ctx, $OUTPUT_DIR . '/admin_regions.json' );
 
 
 foreach my $avwThreshold ( 50, 100, 200, 400, 800, 1600, 3200 ){

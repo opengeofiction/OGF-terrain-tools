dde24503595f37a51e496e871593ddd740010b72
wangi <lee.kindness@inovageo.com>
2022-07-21 13:35:38 +0100

    collate backup files, only do all weekly - XML only otherwise

--- bin/backupWiki.sh
+++ bin/backupWiki.sh
@@ -10,7 +10,7 @@ BACKUP_QUEUE=/opt/opengeofiction/backup-to-s3-queue
 DATEPRE=$(date '+%Y%m%d')
 DATESTR=$(date '+%Y%m%d%H%M')
 
-# daily / weekly / monthly / yearly (only the XML uses this)
+# daily / weekly / monthly / yearly 
 lastthu=$(ncal -h | awk '/Th/ {print $NF}')
 today=$(date +%-d)
 timeframe=daily
@@ -33,22 +33,27 @@ find $DESTDIR -name "backup-$DATEPRE*" -delete -print
 
 cd $SRCDIR
 
-# and do the backups, queue for S3 backup as we go
-echo "MediaWiki dir"
-tar zcf $DESTDIR/backup-$DATESTR-mediawiki.tar.gz --exclude=cache --exclude=images --exclude=util/extracted .
-ln $DESTDIR/backup-$DATESTR-mediawiki.tar.gz ${BACKUP_QUEUE}/daily:wiki:backup-$DATESTR-mediawiki.tar.gz
-
-echo "MediaWiki images dir"
-php maintenance/dumpUploads.php | tar zcfT $DESTDIR/backup-$DATESTR-images.tar.gz -
-ln $DESTDIR/backup-$DATESTR-images.tar.gz ${BACKUP_QUEUE}/daily:wiki:backup-$DATESTR-images.tar.gz
-
+# and do the XML backup, queue for S3 backup
 echo "MediaWiki XML db backup"
 php maintenance/dumpBackup.php --full --quiet | gzip > $DESTDIR/backup-$DATESTR-db.xml.gz
 ln $DESTDIR/backup-$DATESTR-db.xml.gz ${BACKUP_QUEUE}/${timeframe}:wiki:backup-$DATESTR-db.xml.gz
 
-echo "MediaWiki MySQL db backup"
-mysqldump -u ogfwikiguy --password='XXXXXXXXXXXXXXXXXXX' ogf_wikiwiki -c | gzip > $DESTDIR/backup-$DATESTR-db.sql.gz
-ln $DESTDIR/backup-$DATESTR-db.sql.gz ${BACKUP_QUEUE}/daily:wiki:backup-$DATESTR-db.sql.gz
+# and do the ancillary backups
+if [ ${timeframe} != "daily" ]; then
+	echo "MediaWiki dir"
+	tar zcf $DESTDIR/backup-$DATESTR-mediawiki.tar.gz --exclude=cache --exclude=images --exclude=util/extracted .
+
+	echo "MediaWiki images dir"
+	php maintenance/dumpUploads.php | tar zcfT $DESTDIR/backup-$DATESTR-images.tar.gz -
+
+	echo "MediaWiki MySQL db backup"
+	mysqldump -u ogfwikiguy --password='XXXXXXXXXXXXXXXXXXX' ogf_wikiwiki -c | gzip > $DESTDIR/backup-$DATESTR-db.sql.gz
+	
+	# collate and queue for S3 backup
+	echo "Collate MediaWiki dir, MediaWiki images dir and MediaWiki MySQL db backup"
+	tar zcf $DESTDIR/backup-$DATESTR-ancillary.tar.gz $DESTDIR/backup-$DATESTR-mediawiki.tar.gz $DESTDIR/backup-$DATESTR-images.tar.gz $DESTDIR/backup-$DATESTR-db.sql.gz
+	ln $DESTDIR/backup-$DATESTR-ancillary.tar.gz ${BACKUP_QUEUE}/${timeframe}:wiki:backup-$DATESTR-ancillary.tar.gz
+fi
 
 echo "Backups:"
 ls -lh $DESTDIR
